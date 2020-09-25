e91c3187657596ec42510de5407565c7756ecec3
Joanny Raby <Joanny.Raby@USherbrooke.ca>
2020-09-25 13:18:50 -0400

    Change docstrings w prof modifs

--- TP1/prog/solution_regression.py
+++ TP1/prog/solution_regression.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 #####
-# VosNoms (Matricule) .~= À MODIFIER =~.
+# Joanny Raby (15062245)
+# Dona Chadid ()
 ###
 
 import numpy as np
@@ -17,14 +18,14 @@ class Regression:
 
     def fonction_base_polynomiale(self, x):
         """
-        Fonction de base qui projette la donnee x vers un espace polynomial tel que mentionne au chapitre 3.
-        Si x est un scalaire, alors phi_x sera un vecteur à self.M dimensions : (x^1,x^2,...,x^self.M)
-        Si x est un vecteur de N scalaires, alors phi_x sera un tableau 2D de taille NxM
+        Fonction de base qui projette la donnee x vers un espace polynomial tel que mentionné au chapitre 3.
+        --> Si x est un scalaire, alors phi_x sera un vecteur de longueur self.M + 1 (incluant le biais) :
+        (1, x^1,x^2,...,x^self.M)
+        --> Si x est un vecteur de N scalaires, alors phi_x sera un tableau 2D de taille Nx(M+1) (incluant le biais)
 
         NOTE : En mettant phi_x = x, on a une fonction de base lineaire qui fonctionne pour une regression lineaire
         """
         # TODO : inclure x_0 = 1 pour chaque donnee dans la projection, lorsque le biais est inclus dans self.w, donc avoir tableau de taille M+1
-        # TODO make it work for m=0? use a warning? pt juste ignorer M=0? a voir avec correcteur. Commentaire dans entrainement dit que m=0 implique recherche hyperparam
         try:
             # on place vecteur 1D dans un tableau 2D
             phi = x.reshape(-1, 1)
@@ -39,8 +40,18 @@ class Regression:
 
     def recherche_hyperparametre(self, X, t):
         """
-        Validation croisee de type "k-fold" pour k=10 utilisee pour trouver la meilleure valeur pour
-        l'hyper-parametre self.M.
+        Trouver la meilleure valeur pour l'hyper-parametre self.M (pour un lambda fixe donné en entrée).
+
+        Option 1
+        Validation croisée de type "k-fold" avec k=10. La méthode array_split de numpy peut être utlisée
+        pour diviser les données en "k" parties. Si le nombre de données en entrée N est plus petit que "k",
+        k devient égal à N. Il est important de mélanger les données ("shuffle") avant de les sous-diviser
+        en "k" parties.
+
+        Option 2
+        Sous-échantillonage aléatoire avec ratio 80:20 pour Dtrain et Dvalid, avec un nombre de répétition k=10.
+
+        Note:
 
         Le resultat est mis dans la variable self.M
 
@@ -65,11 +76,11 @@ class Regression:
         for val_m in np.arange(1, 10, 1):
             self.M = val_m
             somme_err_valid = 0
-            
+
             # On utilise chaque bloc séparément comme ensemble de validation
             for bloc in np.arange(0, k, 1):
                 debut_valid, fin_valid = selection[bloc]
-                
+
                 # Listes des indices des donnees selectionnees
                 train_indexes = np.concatenate((melange[0:debut_valid], melange[fin_valid:N]))
                 valid_indexes = melange[debut_valid:fin_valid]
@@ -90,7 +101,7 @@ class Regression:
         # 'k - N%k' blocs de taille 'N//k'
         taille_bloc = N // k
         nb_blocs_diff = N % k
-        
+
         # Creation de la liste de selection d'index (index_debut, index_fin)
         bloc_courant = 0
         debut = 0
@@ -121,12 +132,12 @@ class Regression:
         utiliser le poids de regularisation specifie par ``self.lamb``.
 
         Cette methode doit assigner le champs ``self.w`` au vecteur
-        (tableau Numpy 1D) de taille D+1, tel que specifie à la section 3.1.4
+        (tableau Numpy 1D) de taille M+1, tel que specifie à la section 3.1.4
         du livre de Bishop.
-        
-        Lorsque using_sklearn=True, vous devez utiliser la classe "Ridge" de 
+
+        Lorsque using_sklearn=True, vous devez utiliser la classe "Ridge" de
         la librairie sklearn (voir http://scikit-learn.org/stable/modules/linear_model.html)
-        
+
         Lorsque using_sklearn=Fasle, vous devez implementer l'equation 3.28 du
         livre de Bishop. Il est suggere que le calcul de ``self.w`` n'utilise
         pas d'inversion de matrice, mais utilise plutôt une procedure
@@ -136,7 +147,6 @@ class Regression:
         (voir fonction self.fonction_base_polynomiale())
 
         NOTE IMPORTANTE : lorsque self.M <= 0, il faut trouver la bonne valeur de self.M
-
         """
         #AJOUTER CODE ICI
         if self.M <= 0:
