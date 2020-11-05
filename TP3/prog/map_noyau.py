e5872c56b02452583d0fbce9f9abfa907c835f1a
Joanny Raby <Joanny.Raby@USherbrooke.ca>
2020-11-05 17:06:25 -0500

    Fix rbf and predict

--- TP3/prog/map_noyau.py
+++ TP3/prog/map_noyau.py
@@ -29,13 +29,13 @@ class MAPnoyau:
         self.d = d
         self.noyau = noyau
         self.x_train = None
-    
+
     def entrainement(self, x_train, t_train):
         """
         Entraîne une méthode d'apprentissage à noyau de type Maximum a
         posteriori (MAP) avec un terme d'attache aux données de type
         "moindre carrés" et un terme de lissage quadratique (voir
-        Eq.(1.67) et Eq.(6.2) du livre de Bishop).  La variable x_train
+        Eq.(1.67) et Eq.(6.2) du livre de Bishop). La variable x_train
         contient les entrées (un tableau 2D Numpy, où la n-ième rangée
         correspond à l'entrée x_n) et des cibles t_train (un tableau 1D Numpy
         où le n-ième élément correspond à la cible t_n).
@@ -49,36 +49,47 @@ class MAPnoyau:
         l'equation 6.8 du livre de Bishop et garder en mémoire les données
         d'apprentissage dans ``self.x_train``
         """
-        #AJOUTER CODE ICI
-        
         I = np.identity(x_train.shape[0])
-        
+
         # generate Gram matrix
-        K = self.Gram(x_train, x_train)
-                    
+        K = self._gram(x_train, x_train)
+
         # a=(K + λI)−1 t
         self.a = np.dot(np.linalg.inv(K + self.lamb * I), t_train)
         self.x_train = x_train
-    
-    def Gram(self, x, y):
+
+    def _gram(self, x, y):
+        """Compute the kernel function on data arrays x and y. Only takes 2D Numpy arrays.
         
-        if self.noyau == "linear": # 𝑘(𝑥,𝑥′)=𝑥.T 𝑥′
+        If it's used for prediction, the self.x_train needs to be y.
+        """
+        if self.noyau == "lineaire": # 𝑘(𝑥,𝑥′)=𝑥.T 𝑥′
             K = x.dot(y.T)
-            
+
         elif self.noyau == "polynomial": # 𝑘(𝑥,𝑥′)=(𝑥.T 𝑥′+𝑐)**𝑀
             K = (x.dot(y.T) + self.c) ** self.M
-            
-        elif self.noyau == "rbf": # 𝑘(𝑥,𝑥′)=exp⁡(−‖𝑥−𝑥′‖**2 / 2 * 𝜎**2)
-            K = np.zeros((x.shape[0], x.shape[0]))
-            for i in range(x.shape[0]):
-                for j in range(y.shape[0]):
-                    K[i,j] = np.exp(-np.linalg.norm(x[i] - y[j])**2 / (2 * self.sigma_square))
-        
+
+        elif self.noyau == "rbf": # 𝑘(𝑥,𝑥′)=exp(−‖𝑥−𝑥′‖**2 / 2 * 𝜎**2)
+            K = np.zeros((x.shape[0], y.shape[0]))
+
+            # complete Gram matrix case (result has shape nxn)
+            if x.shape == y.shape :
+                for i in range(x.shape[0]):
+                    for j in range(y.shape[0]):
+                        if j >= i :
+                            K[i,j] = np.exp(-np.linalg.norm(x[i] - y[j])**2 / (2 * self.sigma_square**2))
+                K = np.maximum(K, K.T)
+            # prediction case (result has shape nx1)
+            else:
+                for i in range(x.shape[0]):
+                    for j in range(y.shape[0]):
+                        K[i,j] = np.exp(-np.linalg.norm(x[i] - y[j])**2 / (2 * self.sigma_square**2))
+
         else: # 𝑘(𝑥,𝑥′)=tanh(𝑏𝑥𝑇𝑥′+𝑑).
             K = np.tanh(self.b * x.dot(y.T) + self.d)
-            
+
         return K
-        
+
     def prediction(self, x):
         """
         Retourne la prédiction pour une entrée representée par un tableau
@@ -92,10 +103,8 @@ class MAPnoyau:
         classification binaire, la prediction est +1 lorsque y(x)>0.5 et 0
         sinon
         """
-        #AJOUTER CODE ICI
-        
-        k = self.Gram(x, self.x_train)
-        predict = np.dot(k, self.a)
+        k = self._gram(self.x_train, np.array([x]))
+        predict = np.dot(k.T, self.a)
         return int(predict > 0.5)
 
     def erreur(self, t, prediction):
@@ -103,7 +112,6 @@ class MAPnoyau:
         Retourne la différence au carré entre
         la cible ``t`` et la prédiction ``prediction``.
         """
-        # AJOUTER CODE ICI
         return (t-prediction)**2
 
     def validation_croisee(self, x_tab, t_tab):
@@ -111,7 +119,7 @@ class MAPnoyau:
         Cette fonction trouve les meilleurs hyperparametres ``self.sigma_square``,
         ``self.c`` et ``self.M`` (tout dépendant du noyau selectionné) et
         ``self.lamb`` avec une validation croisée de type "k-fold" où k=10 avec les
-        données contenues dans x_tab et t_tab.  Une fois les meilleurs hyperparamètres
+        données contenues dans x_tab et t_tab. Une fois les meilleurs hyperparamètres
         trouvés, le modèle est entraîné une dernière fois.
 
         SUGGESTION: Les valeurs de ``self.sigma_square`` et ``self.lamb`` à explorer vont
