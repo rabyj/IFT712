4773e46e5b68011dc34c2b5afc7cfdea19ed3ad4
dshdid <dona.shdid97@gmail.com>
2020-11-28 19:35:53 +0100

    v6 replace name NB_clf

--- Project/NB_clf.py
+++ Project/NB_clf.py
@@ -19,6 +19,6 @@ class NB_clf(classifier):
         """
         
         super(NB_clf, self).__init__(X_train, X_test, t_train, t_test)
-        self.model = "GaussianNB"
+        self.model = "naive_bayes GaussianNB"
         self.classifier = GaussianNB()
         self.parameters = {'var_smoothing': np.arange(1e-8, 1e-5, 1e-7)}
